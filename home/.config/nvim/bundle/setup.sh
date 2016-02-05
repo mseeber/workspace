4750c72291912c91739c68cb551653249630532b
Markus Seeber <markus.seeber@spectralbird.de>
2016-02-05 15:25:08 +0100

    [config] add installer for some vim plugins
    I do not want to add submodules, since these references can break any
    time. Also, using submodules would cause the check script to check for a
    lot of files which is not nescessary.
    I do not considder the exact internal state of these plugins part of my
    shared configuration, so I do not need to track it.

--- home/.config/nvim/bundle/setup.sh
+++ home/.config/nvim/bundle/setup.sh
@@ -1,14 +1,19 @@
-Plugins
--------
+#!/usr/bin/bash
 
+git clone \
 https://github.com/scrooloose/nerdtree
 
+git clone \
 https://github.com/bling/vim-airline
 
+git clone \
 https://github.com/altercation/vim-colors-solarized
 
+git clone \
 https://github.com/tpope/vim-fugitive
 
-https://github.com/vim-scripts/vim-webdevicons
-
+git clone \
 https://github.com/airblade/vim-gitgutter
+
+git clone \
+https://github.com/wikimatze/vim-webdevicons.git
