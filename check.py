4c54eb2103c2e61d9cdadc0e72d67917634670da
Markus Seeber <markus.seeber@spectralbird.de>
2016-02-05 16:21:31 +0100

    [tools] refactor check.py for more dynamic reference home

--- check.py
+++ check.py
@@ -14,16 +14,23 @@ FILE_MODIFIED = "\033[36m[M]\033[0m"
 
 print("Staus of your home:\n",
       "[F] file found\n",
-      "[M] file is modified\n"
+      "[M] file is modified\n",
       "[?] file missing\n",
      )
 
-for root, dirs, files in os.walk(".", topdown=True):
+target_home = os.path.expanduser('~')
+reference_home= './home'
+
+for path, dirs, files in os.walk(reference_home, topdown=True):
+    # slice away the reference home location
+    target_path = path[len(reference_home)+1:]
+
     for file in files:
-        expected_file = os.path.join(os.path.expanduser('~'), root, file)
+        expected_file = os.path.join(target_home, target_path, file)
         expected_file = os.path.abspath(expected_file)
+
         if os.path.exists(expected_file):
-            repo_file = os.path.join(root, file)
+            repo_file = os.path.join(path, file)
             if filecmp.cmp(expected_file, repo_file):
                 print("{0} {1}".format(FILE_FOUND, expected_file))
             else:
