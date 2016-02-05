b19b6b9c9ccb459c37a6fe17e5f9078436b1062a
Markus Seeber <markus.seeber@spectralbird.de>
2016-02-05 14:39:03 +0100

    [tools] implement checking for file differences
    With this, the user can now see also if a configuration file differs
    from the repo.

--- home/check.py
+++ home/check.py
@@ -2,9 +2,11 @@
 
 import os
 import glob
+import filecmp
 
-file_found = "\033[36m[F]\033[0m"
+file_found = "\033[34m[F]\033[0m"
 file_missing = "\033[33m[?]\033[0m"
+file_modified = "\033[36m[M]\033[0m"
 
 print("Staus of your home:\n",
     "[?] file missing\n",
@@ -16,6 +18,10 @@ for root, dirs, files in os.walk(".", topdown=True):
         expected_file = os.path.join(os.path.expanduser('~'), root, file)
         expected_file = os.path.abspath(expected_file)
         if os.path.exists(expected_file):
-            print("{0} {1}".format(file_found, expected_file))
+            repo_file = os.path.join(root, file)
+            if filecmp.cmp(expected_file, repo_file):
+                print("{0} {1}".format(file_found, expected_file))
+            else:
+               	print("{0} {1}".format(file_modified, expected_file))
         else:
             print("{0} {1}".format(file_missing, expected_file))
