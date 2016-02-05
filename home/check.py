110719d32b8fbe0d257102bc49ec5795a96c1b62
Markus Seeber <markus.seeber@spectralbird.de>
2016-02-05 14:18:37 +0100

    [tools] refactor check script to honor dotfiles
    Python glob does not match filenames beginning with dot
    per default. Also unifying the loop does not require to keep a huge list
    of strings in memory, in case this repository gets very big.

--- home/check.py
+++ home/check.py
@@ -11,11 +11,11 @@ print("Staus of your home:\n",
 	"[F] file found\n",
 	"[M] file is modified\n")
 
-repo_files = glob.glob("**", recursive=True)
-
-for f in repo_files:
-	expected_file = os.path.join(os.path.expanduser('~'), f)
-	if os.path.exists(expected_file):
-		print("{0} {1}".format(file_found, expected_file))
-	else:
-		print("{0} {1}".format(file_missing, expected_file))
+for root, dirs, files in os.walk(".", topdown=True):
+	for file in files:
+		expected_file = os.path.join(os.path.expanduser('~'), root, file)
+		expected_file = os.path.abspath(expected_file)
+		if os.path.exists(expected_file):
+			print("{0} {1}".format(file_found, expected_file))
+		else:
+			print("{0} {1}".format(file_missing, expected_file))
