0d3e29e595f1bdad96971a9c121643458107465b
Markus Seeber <markus.seeber@spectralbird.de>
2016-02-05 14:24:08 +0100

    [tools] tabs to spaces in check.py

--- home/check.py
+++ home/check.py
@@ -7,15 +7,15 @@ file_found = "\033[36m[F]\033[0m"
 file_missing = "\033[33m[?]\033[0m"
 
 print("Staus of your home:\n",
-	"[?] file missing\n",
-	"[F] file found\n",
-	"[M] file is modified\n")
+    "[?] file missing\n",
+    "[F] file found\n",
+    "[M] file is modified\n")
 
 for root, dirs, files in os.walk(".", topdown=True):
-	for file in files:
-		expected_file = os.path.join(os.path.expanduser('~'), root, file)
-		expected_file = os.path.abspath(expected_file)
-		if os.path.exists(expected_file):
-			print("{0} {1}".format(file_found, expected_file))
-		else:
-			print("{0} {1}".format(file_missing, expected_file))
+    for file in files:
+        expected_file = os.path.join(os.path.expanduser('~'), root, file)
+        expected_file = os.path.abspath(expected_file)
+        if os.path.exists(expected_file):
+            print("{0} {1}".format(file_found, expected_file))
+        else:
+            print("{0} {1}".format(file_missing, expected_file))
