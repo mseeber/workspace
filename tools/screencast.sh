fda17d9e294df96218e7a172e2f36d5eee864ead
Markus Seeber <markus.seeber@spectralbird.de>
2016-02-06 17:56:36 +0100

    [tools] rework for screencast script
    Pulseaudio is very flaky for this kind of setup, there are often
    situations where AV goes horribly out of sync. If a device goes
    idle there are a lot of warnings. There needs to be
    some way to either do SRC directly or capture streams separately
    and resample and merge afterwards manually.

--- tools/screencast.sh
+++ tools/screencast.sh
@@ -3,13 +3,31 @@
 # not so simple one-liner for capturing video and audio for a screencast
 # the state of this is "works for me"
 
-ffmpeg -thread_queue_size 64 \ # avoid some race conditions at startup
-	-loglevel error \ # this avoids unneeded spam on console
-	-video_size 1920x1080 \
-	-framerate 25 \
-	-f x11grab -i :0.0 \
-	-f pulse -i alsa_output.pci-0000_00_1b.0.analog-stereo.monitor \
-	-af aresample=async=1 \ #avoid bigger audio artifacts and asyncronous AV at cost of samplerate conversion
-	-c:v libx264 -crf 0 -preset ultrafast \ #do not waste too much CPU cycles on coding
-	/dev/shm/recapture.mkv #TODO more sane location for output file
+TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
+FILENAME="cast.${TIMESTAMP}.mkv"
+TARGETDIR="${HOME}/videos/"
 
+#screen
+FPS=30
+RES="1920x1080"
+XDISPLAY=":0.0"
+
+#sound
+MONITOR="alsa_output.pci-0000_00_1f.3.analog-stereo.monitor"
+MIC="alsa_input.pci-0000_00_1f.3.analog-stereo"
+
+#coder
+CRF=0  # 0=lossless 50=maximum loss, below ~20 perceptionally lossless
+PRESET="ultrafast"  # let's do not waste time here to save some bytes
+
+set -x
+ffmpeg \
+	-thread_queue_size 64 \
+	-video_size ${RES}\
+	-framerate ${FPS} \
+	-f x11grab -r ${FPS} -i ${XDISPLAY} \
+	-f pulse -i ${MIC} \
+	-f pulse -i ${MONITOR} \
+	-filter_complex amerge \
+	-c:v libx264 -crf ${CRF} -preset ${PRESET} \
+	-y "${TARGETDIR}${FILENAME}"
