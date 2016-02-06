564ea078545d15cb2fc3b42228f350da74219920
Markus Seeber <markus.seeber@spectralbird.de>
2016-02-06 19:03:16 +0100

    [tools] factor out status printing and add symlink tag

--- check.py
+++ check.py
@@ -8,15 +8,20 @@ repository.
 import os
 import filecmp
 
-FILE_FOUND = "\033[34m[F]\033[0m"
-FILE_MISSING = "\033[33m[?]\033[0m"
-FILE_MODIFIED = "\033[36m[M]\033[0m"
+FILE_FOUND = "\033[34m[F]\033[0m"      # blue
+FILE_MISSING = "\033[33m[?]\033[0m"    # yellow
+FILE_MODIFIED = "\033[36m[M]\033[0m"   # cyan
+FILE_SYMLINK = "\033[35m[S]\033[0m"    # cyan
 
 print("Staus of your home:\n",
       "[F] file found\n",
       "[M] file is modified\n",
+      "[S] file is symlink\n",
       "[?] file missing\n",
      )
+
+def print_file_state(state, file_name):
+    print("{0} {1}".format(state, file_name))
 
 target_home = os.path.expanduser('~')
 reference_home= './home'
@@ -32,8 +37,8 @@ for path, dirs, files in os.walk(reference_home, topdown=True):
         if os.path.exists(expected_file):
             repo_file = os.path.join(path, file)
             if filecmp.cmp(expected_file, repo_file):
-                print("{0} {1}".format(FILE_FOUND, expected_file))
+                print_file_state(FILE_FOUND, expected_file)
             else:
-                print("{0} {1}".format(FILE_MODIFIED, expected_file))
+                print_file_state(FILE_MODIFIED, expected_file)
         else:
-            print("{0} {1}".format(FILE_MISSING, expected_file))
+            print_file_state(FILE_MISSING, expected_file)
