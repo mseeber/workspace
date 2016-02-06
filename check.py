795025138e453eaaad57f45e6b3c501291374511
Markus Seeber <markus.seeber@spectralbird.de>
2016-02-06 19:52:03 +0100

    [tools] check.py refactor and cleanup
    - turns out, that removing the extra print function removes the need to
      pass the path twice (see before) and pass the state around.
    
    - renaming and documenting for more clarity

--- check.py
+++ check.py
@@ -13,19 +13,20 @@ FILE_MISSING = "\033[33m[?]\033[0m"    # yellow
 FILE_MODIFIED = "\033[36m[M]\033[0m"   # cyan
 FILE_SYMLINK = "\033[35m[S]\033[0m"    # cyan
 
-def print_file_state(file_name, state):
-    print("{0} {1}".format(state, file_name))
-
-def get_file_state(target_file, repo_file):
+def print_file_state(target_file, reference_file):
+    """
+    Print an indicator on console on how the target_file differs from
+    reference_file.
+    """
     if os.path.exists(target_file):
-        if filecmp.cmp(target_file, repo_file):
+        if filecmp.cmp(target_file, reference_file):
             state = FILE_FOUND
         else:
             state = FILE_MODIFIED
     else:
         state = FILE_MISSING
 
-    return state
+    print("{0} {1}".format(state, reference_file))
 
 print("Staus of your home:\n",
       "[F] file found\n",
@@ -46,6 +47,5 @@ for path, dirs, files in os.walk(REFERENCE_HOME, topdown=True):
         expected_file = os.path.join(TARGET_HOME, target_path, file)
         expected_file = os.path.abspath(expected_file)
         repo_file = os.path.join(path, file)
-        file_state = get_file_state(expected_file, repo_file)
-        print_file_state(expected_file, file_state)
+        print_file_state(expected_file, repo_file)
 
